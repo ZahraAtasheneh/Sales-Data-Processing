83a013c0893e0e5822975b7bbdf1f4d548b587bd
Zara Atasheneh <97232164+ZahraAtasheneh@users.noreply.github.com>
2023-07-04 23:16:23 +0300

    main.py

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from sales_processor import process_sales_data
 
-# Specifing the path to the sales data CSV file
+# Specifing the path to the sales data CSV file(sales_data.csv is the name of the file where the sales data is stored)
 csv_file = 'sales_data.csv'
 
-# Processing the sales data
+# Processing the sales data(doing the calculations using the sales data from the file)
 total_sales = process_sales_data(csv_file)
 
 # Displaying the total sales
 print(f'Total sales: ${total_sales:.2f}')
+#The .2 is a formatting specifier. ' :.2f' means it's a float number with two decimal places. The number is rounded to two decimal places.
